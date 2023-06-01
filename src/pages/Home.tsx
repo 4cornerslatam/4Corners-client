02d6657122803dd07aa0657f9cc91edccc4205c3
Kamila Ojeda <beau.ojeda@gmail.com>
2023-06-01 16:39:26 -0300

    Home page layout implementation.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 function Home() {
-    return(
-        <h1>Home</h1>
-    )
+	return (
+		<section className='home'>
+			<div className='home-title'>
+				<p className='linea-1'>Coding</p>
+				<p className='linea-2'>For</p>
+				<p className='linea-3'>Dreams</p>
+			</div>
+			<div className='home-paragraph'>
+				<p>
+					Queremos ser tus aliadas y facilitar tu camino en el mundo digital. Por eso
+					nos esforzamos por entender tus necesidades y diseñar soluciones intuitivas
+					y accesibles que mejoren la vida de las personas.
+				</p>
+			</div>
+			<div className='home-button'>
+				<button>Saber +</button>
+			</div>
+		</section>
+	);
 }
 
-export { Home }
+export { Home };
