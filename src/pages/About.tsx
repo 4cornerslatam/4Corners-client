cec1fa74e82b2abb289ffb90378029f3c3d5208c
Ingrid G <ing.gape18@gmail.com>
2023-06-05 10:46:58 -0500

    maquetado intro en about us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,44 @@
-function About() {
+import { ButtonToServices } from '../components/ButtonToServices/ButtonToServices'
+import data from '../translation/spanish.json'
+
+const About = (): JSX.Element => {
+    const about = data.aboutUs
+    
     return (
-        <h1>About</h1>
+        <div className='about'>
+            <div className='about-info'>
+                <section className='title'>
+                    <h1>{about.TITLE.titleLine1}</h1>
+                    <h1>{about.TITLE.titleLine2.toLocaleLowerCase()}</h1>
+                </section>
+                <section className='description'>
+                    <p>{about.DESCRIPTION}</p>
+                </section>
+                <section className='button'>
+                    <ButtonToServices />
+                </section>
+            </div>
+            <div className='about-values'>
+                <section className='title'>
+                    {about.VALUES.map((item) => {
+                        return (
+                            <div>
+                                <h2>{item.title}</h2>
+                                <section className='description'>
+                                    <p>{item.description}</p>
+                                    <figure className='icon'>
+                                        <img alt='nav'/>
+                                    </figure>
+                                </section>
+                            </div>
+                        )
+                    })}
+                </section>
+                
+
+
+            </div>
+        </div>
     )
 }
 
