5ab1453b4641f8870ca16b7382ec603f55118ddf
Ingrid G <ing.gape18@gmail.com>
2023-06-05 18:54:54 -0500

    Text transition and Icon needed

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,31 +1,30 @@
 import { useState } from 'react'
 import data from '../../translation/spanish.json'
 
-
-const Carousel = () :JSX.Element => {
-    const [index, setIndex] = useState<number>(0); 
+const CarouselComponent = (): JSX.Element => {
+    const [index, setIndex] = useState<number>(0);
     const length = 4;
-    const items = data.aboutUs.VALUES 
+    const items = data.aboutUs.VALUES;
     
     const handleNext = () => {
-    const newIndex = index + 1;
-    setIndex(newIndex >= length ? 0 : newIndex);
-  };
+        const newIndex = index + 1;
+        setIndex(newIndex >= length ? 0 : newIndex);
+    };
 
-  return (
-    <div className='carousel'>
-        <div className='carousel-title'>
-            <h1>{items[index].title}</h1>
+    return (
+        <div className='carousel'>
+            <div className='carousel-title'>
+                <h1>{items[index].title}</h1>
+            </div>
+            <div className='carousel-description'>
+                <p>{items[index].description}</p>
+                <button onClick={handleNext}>
+                    <span>+</span>
+                </button>
+            </div>
         </div>
-        <div className='carousel-description'>
-            <p>{items[index].description}</p>
-            <button onClick={handleNext}>
-            </button>
-        </div>
-    </div>
-  );
-};
+    )
+}
 
 //https://www.makeuseof.com/react-js-interactive-carousel-build/
-
-export default Carousel;
+export default CarouselComponent
