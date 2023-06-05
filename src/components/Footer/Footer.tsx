cbcc6a4a98391faaa86b6ec8e6881c03dda529e0
beresdev <beresdev@gmail.com>
2023-06-05 10:16:59 -0500

    footer in mobile done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from '../../context/AppContext';
 
 function Footer () {
-    const { baseColor} = useContext(AppContext)
+    const { showNavbar, baseColor, setBaseColor } = useContext(AppContext)
+    const [brandColor, setBrandColor] = useState(baseColor)
     
+    useEffect(() => {
+        setBaseColor(showNavbar ? '#C54F81' : '#FCFCFC' );
+        setBrandColor(showNavbar ? '#FCFCFC' : '#C54F81')
+    }, [showNavbar, setBaseColor])
+
     return(
         <footer style={{backgroundColor: baseColor}}>
-            I'm the footer
+          <div className="social-media_container" >
+            <i className="fa-brands fa-linkedin-in" style={{color: brandColor}}></i>
+            <i className="fa-brands fa-instagram" style={{color: brandColor}}></i>
+            <i className="fa-brands fa-youtube" style={{color: brandColor}}></i>
+          </div>
+          <div className="copyright-container">
+            <i className="fa-regular fa-copyright"></i>
+            <p>2023 - 4corners</p>
+          </div>
         </footer>
     )
 }
