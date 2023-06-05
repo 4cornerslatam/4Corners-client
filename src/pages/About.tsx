a4460361b941bcac9065f2e420a8ffdf9084dbc0
Kamila Ojeda <beau.ojeda@gmail.com>
2023-06-05 12:53:37 -0500

    Adds the layout of the About page.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,23 @@
-function About() {
-    return (
-        <h1>About</h1>
-    )
-}
+import { ButtonToServices } from '../components/ButtonToServices/ButtonToServices';
+import data from '../translation/spanish.json';
 
-export { About }
+const About = (): JSX.Element => {
+	const team = data.team;
+
+	return (
+		<section className='team'>
+			<div className='team-title'>
+				<p className='linea-1'>{team.TITLE.titleLine1}</p>
+				<p className='linea-2'>{team.TITLE.titleLine2}</p>
+			</div>
+			<div className='team-paragraph'>
+				<p>{team.DESCRIPTION}</p>
+			</div>
+			<div className='team-button'>
+				<ButtonToServices />
+			</div>
+		</section>
+	);
+};
+
+export { About };
