ffb23621e6a2e1dbb9ba84e5a13dbb26416c7d9f
Kamila Ojeda <beau.ojeda@gmail.com>
2023-06-07 12:07:32 -0500

    Implements front and back of the  MemberCard component.

--- src/components/MembersCard/MembersCard.tsx
+++ src/components/MembersCard/MembersCard.tsx
@@ -6,8 +6,8 @@ const MembersCard = (): JSX.Element => {
 	return (
 		<section>
 			<div className='container'>
-				<div className='content'>
-					<div className='card'>
+				<div className='card card-front'>
+					<div className='content'>
 						<div className='card-content'>
 							<div className='card-image'>
 								<img src='src/assets/Bere.png' alt='Berenice Hernandez' />
@@ -26,24 +26,24 @@ const MembersCard = (): JSX.Element => {
 							</div>
 						</div>
 					</div>
+					<div className='card card-back'>
+						<div className='card-content-back'>
+							<div className='card-icons'>
+								<i className='fa-solid fa-xmark'></i>
+							</div>
+							<div className='card-image'>
+								<img src='src/assets/Bere.png' alt='Berenice Hernandez' />
+							</div>
+							<div className='card-description'>
+								<span className='name'>{team.TEAM[0].name}</span>
+
+								<span className='description'>{team.TEAM[0].description}</span>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
-		// <section className='card-container'>
-		// 	<div className='card-up'>
-		// 		<a href='https://www.linkedin.com/in/beresdev/' target='_blank'>
-		// 			<i className='fa-brands fa-linkedin-in'></i>
-		// 		</a>
-		// 	</div>
-
-		// 	<div className='card-down'>
-		// 		<div className='card-img'></div>
-
-		// 		<p className='card-name'>{team.TEAM[1].name}</p>
-		// 		<p></p>
-		// 		<button></button>
-		// 	</div>
-		// </section>
 	);
 };
 
